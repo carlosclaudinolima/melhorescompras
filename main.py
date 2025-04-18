28389b45c7fc8e4c0287057f8200fc25f3ae4eb0
Carlos André Claudino de Lima <carlosclaudinolima@gmail.com>
2025-04-17 23:19:39 -0300

    inicio do desenvolvimento

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 sistema_operacional = os.name
+dc_produtos = {}
 
 def limpa_tela():
     if sistema_operacional == 'nt':
@@ -20,9 +21,20 @@ def banner():
     
 
 
+def cadastrar_produto():
+    numero_produto = len(dc_produtos)+1
+    nome_produto = ''
+    while nome_produto == '':
+        nome_produto = input(f'Digite o nome do produto #{numero_produto} ')
+        print(nome_produto)
+
 def main():
     banner()
-    #Aqui entra a piroca
+    while True:
+        cadastrar_produto()
+        if input("Deseja cadastrar mais um produto ? (S/N) ").upper() != 'S':
+            break
+
 
 if __name__ == '__main__':
     main()
